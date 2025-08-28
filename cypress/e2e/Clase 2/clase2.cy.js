a9b6c6991e79335b08127977b67de593b88b2c2a
Maximiliano Gabelas Miño <maxigm87@gmail.com>
2025-08-28 04:22:41 -0300

    Segundo cambio

--- cypress/e2e/Clase 2/clase2.cy.js
+++ cypress/e2e/Clase 2/clase2.cy.js
@@ -2,7 +2,7 @@ describe('HappyPath - Dirección a UI', ()=> {
 it('Dirección a UI', () => { 
 cy.visit ('https://endearing-lollipop-19f16d.netlify.app/')
 cy.get('[data-cy="nav-interacciones-ui"]').click() 
-cy.get('[data-cy="email-input"]').type('cualquier@gmail.com') 
+cy.get('[data-cy="email-input"]').type('cual@gmail.com') 
 })
 
 })
